acdc3e9f2f054fefebf24eba797baa4f2fbc13a9
Andrew Reitz <aj.reitz@gmail.com>
2014-08-30 13:18:14 -0500

    Updated Archetype

---
+++ velcro-archetype/src/main/resources/archetype-resources/src/main/java/ui/ActivityModule.java
@@ -0,0 +1,29 @@
+package ${package}.ui;
+
+import android.app.Activity;
+
+import ${package}.${applicationName}Module;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module(
+    injects = {
+        MainActivity.class
+    },
+    addsTo = ${applicationName}Module.class,
+    library = true
+)
+public class ActivityModule {
+  private final Activity activity;
+
+  public ActivityModule(Activity activity) {
+    this.activity = activity;
+  }
+
+  @Provides @Singleton Activity provideActivityContext() {
+    return activity;
+  }
+}
