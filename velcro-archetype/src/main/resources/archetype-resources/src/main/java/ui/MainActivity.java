1707401aad7deef1ea7b609a00152bf641976e45
Andrew Reitz <aj.reitz@gmail.com>
2014-08-30 16:10:36 -0500

    Testing release by adding back in sources

---
+++ velcro-archetype/src/main/resources/archetype-resources/src/main/java/ui/MainActivity.java
@@ -0,0 +1,25 @@
+package ${package}.ui;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import ${package}.R;
+import ${package}.${applicationName}App;
+import ${package}.ui.misc.BaseActivity;
+
+import javax.inject.Inject;
+
+public class MainActivity extends BaseActivity {
+
+  @Inject AppContainer appContainer;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    ${applicationName}App app = ${applicationName}App.get(this);
+    ViewGroup container = appContainer.get(this, app);
+
+    getLayoutInflater().inflate(R.layout.activity_main, container);
+  }
+}
